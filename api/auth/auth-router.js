2c43d8a04107187b531f04c893cca2ee8804c103
Anil_Ramcharran <anil.gitme@gmail.com>
2021-04-18 19:59:51 -0400

    testing

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -23,7 +23,6 @@ async function addUser(user) {
     return newUser;
 }
 
-
 router.post('/register', async(req, res, next) => {
     // res.end('implement register, please!');
     /*
@@ -103,7 +102,7 @@ router.post('/login', async(req, res) => {
             let checkPass = bcrypt.compareSync(password, user.password);
             if (checkPass) {
                 jwt.sign({ username: user.username }, jwtSecret, (err, token) => {
-                    return res.status(200).json({ message: `welcome, ${user.username}`, token })
+                    return res.status(200).json({ message: `welcome, ${user.username}`, token: 'token' })
                 })
             } else {
                 return res.status(400).json({ message: 'invalid credentials' })
