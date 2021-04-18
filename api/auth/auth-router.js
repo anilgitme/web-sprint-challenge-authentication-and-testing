53bba9ba28c95a66841255a042d9aaf3e7218936
Anil_Ramcharran <anil.gitme@gmail.com>
2021-04-18 19:03:53 -0400

    debugging error on codegrade

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -102,11 +102,17 @@ router.post('/login', async(req, res) => {
             let checkPass = bcrypt.compareSync(password, user.password);
             if (checkPass) {
                 jwt.sign({ username: user.username }, jwtSecret, (err, token) => {
-                    return res.status(200).json({ message: `welcome, ${user.username}`, token })
+                    if (err) {
+                        return res.status(401).json({ message: 'error logging in' })
+                    } else {
+                        return res.status(200).json({ message: `welcome, ${user.username}`, token })
+                    }
                 })
             } else {
                 return res.status(400).json({ message: 'invalid credentials' })
             }
+        } else {
+            return res.status(400).json({ message: 'invalid credentials' })
         }
     } catch (err) {
         res.status(404).json({ message: 'login failed' })
