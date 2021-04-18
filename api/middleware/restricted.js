c46d12f393d5000e8df9b7db7262d6a15074eb13
Anil_Ramcharran <anil.gitme@gmail.com>
2021-04-18 19:28:41 -0400

    testing

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -3,17 +3,17 @@ const jwtSecret = require('../../config/secrets');
 
 module.exports = (req, res, next) => {
     const token = req.headers.Authorization;
-    if (!token) {
-        res.status(404).json({ message: 'token required' })
-    } else {
+    if (token) {
         jwt.verify(token, jwtSecret, (err, decoded) => {
             if (err) {
                 res.status(404).json({ message: 'token invalid' })
             } else {
-                req.decoded = decoded;
+                req.decodedJWT = decoded; //saves the token in the req
                 next();
             }
         })
+    } else {
+        res.status(401).json({ message: 'token required' })
     }
     // next();
     /*
