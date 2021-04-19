1a81a7a3c97ed85938ce50809e6f015e7fb2990a
Anil_Ramcharran <anil.gitme@gmail.com>
2021-04-18 20:09:31 -0400

    mvpeed

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -102,7 +102,7 @@ router.post('/login', async(req, res) => {
             let checkPass = bcrypt.compareSync(password, user.password);
             if (checkPass) {
                 jwt.sign({ username: user.username }, jwtSecret, (err, token) => {
-                    return res.status(200).json({ message: `welcome, ${username}`, token })
+                    return res.status(200).json({ message: `welcome, ${username}`, token: 'token' })
                 })
             } else {
                 return res.status(400).json({ message: 'invalid credentials' })
